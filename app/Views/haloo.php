34a1f86e8c80c4a069006f6f672435bd7921d8d8
Marcell Bintang Setiawan <bintangsetiawan900@gmail.com>
2022-05-12 22:25:00 +0700

    mengubah direktori css & js

--- app/Views/haloo.php
+++ app/Views/haloo.php
@@ -5,12 +5,12 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>/style.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <title>E - SPM | Sistem Pengaduan Mahasiswa Online</title>
 </head>
 
 <body id="body-pd">
@@ -61,7 +61,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
   -->
-  <script src="<?php echo base_url(); ?>/script.js"></script>
+  <script src="<?php echo base_url(); ?>/assets/js/script.js"></script>
 </body>
 
 </html>
---
+++ public/assets/js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  const showNavbar = (toggleId, navId, bodyId, headerId, imgId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId),
+    imgpd = document.getElementById(imgId)
+
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd && imgpd){
+      toggle.addEventListener('click', ()=>{
+      // show navbar
+      nav.classList.toggle('show')
+      // change icon
+      toggle.classList.toggle('bx-x')
+      // add padding to body
+      bodypd.classList.toggle('body-pd')
+      // add padding to header
+      headerpd.classList.toggle('body-pd')
+      
+      imgpd.classList.toggle('imgpd')
+      imgpd.classList.toggle('text-center')
+
+
+      })
+    }
+  }
+  
+  showNavbar('header-toggle','nav-bar','body-pd','header','imgpd')
+  
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll('.nav_link')
+  
+  function colorLink(){
+    if(linkColor){
+      linkColor.forEach(l=> l.classList.remove('active'))
+      this.classList.add('active')
+    }
+  }
+
+  linkColor.forEach(l=> l.addEventListener('click', colorLink))
+  
+  // Your code to run since DOM is loaded and ready
+  });
