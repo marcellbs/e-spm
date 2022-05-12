088ec7d9468b2d4213ef563a9cb67ed0e5b43ba6
Marcell Bintang Setiawan <bintangsetiawan900@gmail.com>
2022-05-12 22:14:33 +0700

    mengubah tampilan layout

--- app/Views/haloo.php
+++ app/Views/haloo.php
@@ -19,14 +19,15 @@
     <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
     <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
   </header>
-
+  
   <div class="l-navbar" id="nav-bar">
     <nav class="nav">
       <div> 
-        <a href="#" class="nav_logo"><i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
+        <a href="#" class="nav_logo"><i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">E - SPM</span> </a>
         
-        <div class="nav_list mt-5"> 
-         
+        <div class="nav_list mt-3"> 
+          
+          <div class="header_img_1 " id="imgpd"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
           
           <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> 
           
